030e21fdb98fb1dc45a895a45abd4f73d9bb4f61
shiful33 <shwapon.joti@gmail.com>
2025-09-04 00:23:22 +0600

    all added lesson section and script.js up to date

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const loadLessons = () => {
+    fetch('https://openapi.programming-hero.com/api/levels/all') // promise of response
+
+    .then(res => res.json()) // promise of json data
+    .then((json) => displayLesson(json.data));
+};
+
+const displayLesson = (lessons) => {
+    
+    const levelContainer = document.getElementById('level-container');
+    levelContainer.innerHTML = '';
+
+    for (let lesson of lessons) {
+        const btnDiv = document.createElement('div');
+        btnDiv.innerHTML = `
+        <button href="#" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no} </button>
+        `;
+        levelContainer.append(btnDiv);
+    }
+};
+
+loadLessons();
